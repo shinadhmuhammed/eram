987d6c274260eb222b5aa6070aadae0888301ffc
Thasni <thasnimaskeva.io@gmail.com>
2025-05-29 16:35:05 +0530

    env value changes

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use("/api/users", userRouter);
 app.use("/api/admin", userRouter);
 
