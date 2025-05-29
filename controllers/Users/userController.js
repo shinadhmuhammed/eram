7c4ff5e3c3b8a4e28d5e488752508a1bb47a3ecc
shinadhmuhammed <muhammedshinad2@gmail.com>
2025-05-29 12:40:26 +0530

    CORS Error fixed

--- controllers/Users/userController.js
+++ controllers/Users/userController.js
@@ -5,9 +5,9 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
+  console.log('hlooooioioio')
   try {
     const { firstName,lastName,fullName,role, email,phone,cPassword } = req.body;
-
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
