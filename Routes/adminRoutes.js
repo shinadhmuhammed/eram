071ab7d1fd112047d150f46b16a860ddb83e93bd
Thasni <thasnimaskeva.io@gmail.com>
2025-05-29 19:07:19 +0530

    admin,recruiter

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { createBranch,getBranch } = require('../controllers/Branch/branchController')
+const { getAllAdmin,addAdmin } = require('../controllers/Admin/adminController')
 
 const adminroute = express.Router();
 
 
 adminroute.get('/branch',getBranch)
+adminroute.get('/admin',getAllAdmin)
 
 adminroute.post('/branch',createBranch)
+adminroute.post('/admin',addAdmin)
+
 
 
 
