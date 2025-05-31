7c6bed45eece85aa690146c358f861024a248ff4
shinadhmuhammed <muhammedshinad2@gmail.com>
2025-05-31 10:33:59 +0530

    add adminRoute

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./connections/dbConnection");
 const userRouter = require("./Routes/userRoutes");
 const  superadminroute = require('./Routes/superAdminRoutes')
+const adminRouter = require("./Routes/adminRoutes");
 const port = 5000 || process.env.port;
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use("/api/users", userRouter);
 app.use('/api/super-admin',superadminroute)
+app.use("/api/admin", adminRouter);
 
 connectDB()
   .then(() => {
