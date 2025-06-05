a52e778fe2955f7468dfb623419cca689fad10ad
shinadhmuhammed <muhammedshinad2@gmail.com>
2025-06-05 16:06:55 +0530

    modal changes

--- models/userModel.js
+++ models/userModel.js
@@ -3,11 +3,9 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: true,
   },
   lastName: {
     type: String,
-    required: true,
   },
 
   fullName: {
